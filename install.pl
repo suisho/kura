aff9c66960cb6ba3787469b58b32b1dae02868bf
suisho <suisho000000@gmail.com>
2012-12-11 04:20:13 +0900

    Add install script

---
+++ install.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+# うーん。なんだかただ改行してくっつけるだけのスクリプトになってしまった。
+
+use strict;
+
+sub main{
+    my @install = qw(
+        git
+        tig
+        make
+        curl
+        ruby
+        cpanminus
+        npm
+        nginx
+  	    emacs
+  	    fish
+        vim
+        php5
+  	    php-fpm
+		ssh
+		nkf
+		open-vm-tools
+	
+        sysv-rc-conf
+        bash-completion
+        build-essential
+        libssl-dev
+        libreadline6-dev
+        zlib1g-dev
+        checkinstall
+        ibus-mozc
+  	    
+  	    ttf-inconsolata
+        ttf-dejavu
+    );
+    my @npms = qw(
+        node-dev
+    );
+    my @remove = qw(
+    	unity-lens-shopping
+    );
+    #lubuntu-desktop
+  	#apache2
+  	#gnome-shell
+    #vim
+	#gnome-tweak-tool
+    print "start\n";
+    my $_install = join(' ',@install);
+    my $install_cmd = 'sudo apt-get install';
+    system("$install_cmd $_install");
+    
+    my $_npms = join(' ',@npms);
+    system("sudo npm install -g $_npms");
+    
+    my $_remove = join(' ',@remove);
+    my $remove_cmd = 'sudo apt-get remove';
+    system("$remove_cmd $_reove");
+    
+    print "end\n";
+}
+main();
