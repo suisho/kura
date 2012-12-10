95c618cd1c547160f0e85e76cd724487e4427328
suisho <suisho000000@gmail.com>
2012-12-11 04:21:23 +0900

    Add init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install language-pack-ja
+#./mklink.sh
+sudo ./install.pl
+#sudo dpkg-reconfigure locales
+#sudo dpkg-reconfigure locales
+#sudo dpkg-reconfigure tzdata
