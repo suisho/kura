5aad9cf6df105329eebbab0a1f1abfb1898d0328
suisho <suisho000000@gmail.com>
2013-01-02 18:54:10 +0900

    add dropbox_exclude

---
+++ dropbox_exclude.sh
@@ -0,0 +1,18 @@
+dropbox exclude add SublimeText2__
+dropbox exclude add android
+dropbox exclude add bitbucket
+dropbox exclude add c#
+dropbox exclude add chad
+dropbox exclude add chrome ext
+dropbox exclude add htdocs
+dropbox exclude add jointabs
+dropbox exclude add jquery_mobile
+dropbox exclude add js
+dropbox exclude add komodo
+dropbox exclude add mikurima
+dropbox exclude add olive
+dropbox exclude add php
+dropbox exclude add redmine__
+dropbox exclude add wallpaper
+dropbox exclude add windows
+dropbox exclude list
