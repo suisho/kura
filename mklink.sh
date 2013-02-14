fd7fe3d253d2af4ce41f69cd5d75b3526d77c065
suisho <suisho000000@gmail.com>
2013-02-14 17:06:41 +0900

    add tmux file

--- mklink.sh
+++ mklink.sh
@@ -3,6 +3,7 @@ ln -b ./dotfile/gitconfig ~/.gitconfig
 ln -b ./dotfile/gitignore ~/.gitignore
 ln -b ./dotfile/bashrc ~/.bashrc
 ln -b ./dotfile/bash_aliases ~/.bash_aliases
+ln -b ./dotfile/tmux.conf ~/.tmux.conf
 
 . ~/.bashrc
 . ~/.bash_aliases
