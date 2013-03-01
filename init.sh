f12f8f899b8d7f65d8c9d8e0345d53233790ab4e
suisho <suisho000000@gmail.com>
2013-03-01 11:03:23 -0800

    add autopurge

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+sudo apt-get autoremove
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install language-pack-ja
+#sudo apt-get install language-pack-ja
 #./mklink.sh
 sudo ./install.pl
 #sudo dpkg-reconfigure locales
