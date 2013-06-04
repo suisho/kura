a2a7e8ff1dfe02c7bbf41c91f904574a393c7bdd
suisho <suisho000000@gmail.com>
2013-06-05 02:42:57 +0900

    add zshrc

--- chefkura/cookbooks/dotfiles/recipes/default.rb
+++ chefkura/cookbooks/dotfiles/recipes/default.rb
@@ -7,8 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 userdir = File.expand_path("~/", __FILE__)
+
 template "#{userdir}/.bashrc" do
 	action :create
 	mode 0700
 	source "bash.erb"
+end
+
+template "#{userdir}/.zshrc" do
+	action :create
+	mode 0700
+	source "zsh.erb"
 end
