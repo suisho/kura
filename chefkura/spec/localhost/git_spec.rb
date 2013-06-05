2e8ac59f5ca0746668a87c68ec6c2fc5a443e445
suisho <suisho000000@gmail.com>
2013-06-06 03:43:14 +0900

    fix regexp

--- chefkura/spec/localhost/git_spec.rb
+++ chefkura/spec/localhost/git_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe command('git --version')do
   it "version > 1.8" do
     # TODO: 1.8以上にしたい
-    should return_stdout /1\.8/
+    should return_stdout /git version 1\.8/
   end
 end
