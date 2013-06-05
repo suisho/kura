aebd90c2d570a059310dae996a780d16dacaa3aa
suisho <suisho000000@gmail.com>
2013-06-05 23:51:24 +0900

    add reload rc file

--- chefkura/cookbooks/dotfiles/recipes/default.rb
+++ chefkura/cookbooks/dotfiles/recipes/default.rb
@@ -6,16 +6,43 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-userdir = File.expand_path("~/", __FILE__)
 
-template "#{userdir}/.bashrc" do
-	action :create
-	mode 0700
-	source "bash.erb"
+def get_current()
+  case ENV["SHELL"]
+  when /bash/
+    return "bash"
+  when /zsh/
+    return "zsh"
+  end
 end
 
-template "#{userdir}/.zshrc" do
-	action :create
-	mode 0700
-	source "zsh.erb"
+def get_rc(shell)
+  userdir = File.expand_path("~/", __FILE__)
+  case shell
+  when "bash", "zsh"
+    return "#{userdir}/.#{shell}rc"
+  when "fish"
+    return "#{userdir}/.config/fish/fishrc"
+  end
+end
+
+current = get_current()
+current_rc = get_rc(current)
+
+template get_rc("bash") do
+  action :create
+  mode 0700
+  source "bash.erb"
+end
+
+template get_rc("zsh") do
+  action :create
+  mode 0700
+  source "zsh.erb"
+end
+
+ruby_block "reload rc file" do
+  block do
+    system("source #{current_rc}")
+  end
 end
