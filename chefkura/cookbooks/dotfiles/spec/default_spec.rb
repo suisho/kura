324373fb9fa2f5c8046d8644a5bbf986216c959e
suisho <suisho000000@gmail.com>
2013-06-05 23:53:34 +0900

    sepalate spec file

--- chefkura/cookbooks/dotfiles/spec/default_spec.rb
+++ chefkura/cookbooks/dotfiles/spec/default_spec.rb
@@ -9,28 +9,3 @@ describe 'dotfiles::default' do
 end
 
 
-
-
-require ::File.join ::File.dirname(__FILE__), "..", "libraries", "settings"
-describe "library" do
-  describe "bash" do
-  
-    let(:bash) { Bash.new }
-  
-    it "should export value foo is baa " do
-      expect(bash.export("foo","baa")).to eq 'export foo=baa'
-    end
-
-    it "should whitespace item is escaped" do
-      expect(bash.export("foo","baa baz")).to eq 'export foo="baa baz"'
-    end
-
-    it "should add $PATH" do
-      paths = ["/usr/local/bin", "~/bin/"]
-      actual = bash.add_path(paths)
-      expect(actual).to eq 'export PATH=/usr/local/bin:~/bin/:$PATH'
-      expect(paths).to eq ["/usr/local/bin", "~/bin/"]
-    end
-  
-  end
-end
