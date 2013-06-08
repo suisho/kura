4b9c474f54788d0551fd1fcf56d87f24e427ed84
suisho <suisho000000@gmail.com>
2013-06-08 13:06:11 +0900

    add cookbooks

--- chefkura/solo.rb
+++ chefkura/solo.rb
@@ -1,9 +1,14 @@
-file_cache_path           "/tmp/chef-solo"
-file_backup_path           "/tmp/chef-solo-backup"
-cookbook_path   [ File.expand_path("../cookbooks", __FILE__) ]
+file_cache_path          "/tmp/chef-solo"
+file_backup_path         "/tmp/chef-solo-backup"
+cookbook_path([
+  File.expand_path("../cookbooks", __FILE__),
+  File.expand_path("../vendor/cookbooks", __FILE__),
+])
 cache_options({
   :path => "/tmp/chef-solo/cache",
   :skip_expires => true
 })
 
 data_bag_path  File.expand_path("../cookbooks", __FILE__)
+
+client_key     File.expand_path("../key/solo.pem", __FILE__)
